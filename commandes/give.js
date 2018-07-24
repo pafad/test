44862c0efeb8bd1fccc72e958539151429ea028f
pafad <32877302+pafad@users.noreply.github.com>
2018-07-24 04:20:43 +0200

    Update give.js

--- commandes/give.js
+++ commandes/give.js
@@ -35,8 +35,8 @@ const url = "https://api.myjson.com/bins/n1r2y";
                 if (userData[Sender.id].coins > args[2]){
                     message.reply("Tu n'as pas assez de <a:coins:467999444567195651>")
              }else{
-                userData[Sender.id].coins -= args[2];
-                userData[mention.id].coins += args[2];
+                userData[Sender.id].coins --- args[2];
+                userData[mention.id].coins +++ args[2];
                 request({ url: url, method: 'PUT', json: userData})
                 message.reply(`Tu as give ${args[2]} <a:coins:467999444567195651> à ${mention.tag}`)
                 client.users.get(mention.id).send(`Tu as reçu ${args[2]} <a:coins:467999444567195651> de la part de ${Sender.tag}`)
