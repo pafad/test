eace0a82973946ce784e7871bdb97e536e2975b5
pafad <32877302+pafad@users.noreply.github.com>
2018-07-24 03:37:17 +0200

    Update mine.js

--- commandes/mine.js
+++ commandes/mine.js
@@ -21,12 +21,6 @@ module.exports.run = async (client, message) => {
         message.reply("Tu n'es pas enregistré fais !register pour mettre à jour la base.")
         return;
     }else{
-        let r = rn({
-            min: 0,
-            max: mines.length - 1,
-            integer: true
-        });
-    let miness = mines[r];
     if(userData[Sender.id].currentMana === 0){
         message.channel.send("Tu n'as plus de mana. <:chat:469113692492005376>")
     }else{
@@ -34,7 +28,7 @@ module.exports.run = async (client, message) => {
     var nombre = Math.floor(Math.random()*crit)
     userData[Sender.id].currentMana--;
     userData[Sender.id].coins += nombre;
-    message.channel.send(`Tu as gagné ${miness} <a:coins:467999444567195651> [${userData[Sender.id].currentMana}/${userData[Sender.id].manaMax}]`)
+    message.channel.send(`Tu as gagné ${nombre} <a:coins:467999444567195651> [${userData[Sender.id].currentMana}/${userData[Sender.id].manaMax}]`)
     request({ url: url, method: 'PUT', json: userData})
     }
 }
