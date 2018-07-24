14b0c98875a044207db3246aa6f632483be24573
pafad <32877302+pafad@users.noreply.github.com>
2018-07-24 03:17:40 +0200

    Update tr.js

--- commandes/tr.js
+++ commandes/tr.js
@@ -18,7 +18,7 @@ console.log('chargé avec succés')
 
     let Tr = JSON.parse(body);
     if(!Tr) Tr = {};
-    if(!Tr.time) Tr.time = Date.now() + Math.floor(Math.random()+600000);
+    if(!Tr.time) Tr.time = Date.now() + Math.floor(Math.random()+300000);
     if(!Tr.taker) Tr.taker = "personne";
     if(!Tr.servtaker) Tr.servtaker = "pas de serveurs";
     request({ url: trUrl, method: 'PUT', json: Tr})
@@ -35,14 +35,16 @@ if((Tr.time > Date.now()) && (Tr.time !== 0)){
     message.channel.send(`Le trésor n'est pas encore récupérable, il sera récupérable dans ${minutes} minutes et ${seconds} secondes. Votre combo est de : x${userData[Sender.id].combotr}, le dernier trésor à été récupérer par : ${Tr.taker} depuis le serveur : ${Tr.servtaker}`)
     return;
 }else{
+  var toAddC = 50 * userData[Sender.id].combotr;
+  var toAddX = 25 * userData[Sender.id].combotr;
   userData[Sender.id].combotr += 1;
-  userData[Sender.id].xp += 25 * userData[Sender.id].combotr;
-  userData[Sender.id].coins += 50 * userData[Sender.id].combotr;
+  userData[Sender.id].xp += toAddX;
+  userData[Sender.id].coins += toAddC;
+  request({ url: url, method: 'PUT', json: userData});
   Tr.taker = Sender.tag;
   Tr.servtaker = message.guild.name;
   Tr.time = Date.now() + Math.floor(Math.random()*4200000);
   message.channel.send(`${Sender}, Tu as gagné ${50 * userData[Sender.id].combotr} <a:coins:467999444567195651> et ${25 * userData[Sender.id].combotr} <:XP:470615654639337472> [combo: ${userData[Sender.id].combotr}]`);
-  request({ url: url, method: 'PUT', json: userData});
   request({ url: trUrl, method: 'PUT', json: Tr});
 
   const logsurl = "https://api.myjson.com/bins/wjtwa";
