a40bb16f6996f7e4dbbd9d4517ad0b4f91c24a92
pafad <32877302+pafad@users.noreply.github.com>
2018-07-24 10:52:36 +0000

    Update str.js

--- commandes/str.js
+++ commandes/str.js
@@ -1,8 +1,8 @@
 const superagent = require("superagent");
 const request = require("request");
-module.exports.run = async (client, message) => {
-if(!message.author.id === ""){
-
+module.exports.run = async (client, message, args) => {
+if(!message.author.id === "306119836503900161"){
+message.reply("tu n'est pas mon développeur")
 }else{
     const trUrl = "https://api.myjson.com/bins/1buwmq";
     request(trUrl, (err, res, body) => {
@@ -12,14 +12,14 @@ if(!message.author.id === ""){
     let Tr = JSON.parse(body);
     if(args[1] === "s"){
     Tr.time = Date.now() +++ args[0]*1000;
-    
+    request({ url: trUrl, method: 'PUT', json: Tr})
     message.channel.send(`trésor dans ${args[0]} secondes`)
     }
     
     if(args[1] === "min"){
     
     Tr.time = Date.now() +++ args[0]*60000;
-    
+    request({ url: trUrl, method: 'PUT', json: Tr})
     message.channel.send(`Trésor dans ${args[0]} minutes`)
     }
     })
