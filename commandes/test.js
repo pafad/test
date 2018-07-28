a1ee4668866114efe7d88789ed09b95800396dd0
pafad <32877302+pafad@users.noreply.github.com>
2018-07-29 01:25:57 +0200

    Create test.js

---
+++ commandes/test.js
@@ -0,0 +1,45 @@
+module.exports.run = async (client, message, args) => {
+  var globalUsers = 0;
+  var globalMoney = 0;
+  var globalRichest = '';
+  var globalRichest$ = 0;
+  for(var i in userData){
+     if(i.endsWith(client.users.findAll("id", userData).map())){
+       globalMoney += userData[i].coins;
+       globalUsers += 1;
+       if(userData[i].coins > globalRichest$){
+           globalRichest$ = userData[i].coins;
+           globalRichest = userData[i].username;
+       }
+     }
+ }
+ message.channel.send({embed: {
+  color: Math.floor(Math.random() * 16777214) + 1,
+  title:"Stats Global",
+  fields:[
+  {
+      name:"Joueurs",
+      value:globalUsers,
+      inline:true
+  },
+  {
+      name:"points total du jeu",
+      value:globalMoney,
+      inline:false
+  },
+  {
+      name: "Le plus riche:",
+      value: `${globalRichest} avec ${globalRichest$} coins`
+  }
+],
+  timestamp: new Date(),
+  footer:{
+      icon_url:message.author.avatarURL,
+      text:"Stats Global"
+      }
+  }})
+}
+
+module.exports.help = {
+    name: "test"
+  }
