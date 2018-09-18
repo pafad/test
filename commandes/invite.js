02cb45632231c5d0aac5c39b610883da52f945b5
pafad <32877302+pafad@users.noreply.github.com>
2018-09-18 13:06:04 +0000

    Create invite.js

---
+++ commandes/invite.js
@@ -0,0 +1,16 @@
+module.exports.run = async (client,message, args) => {
+  message.channel.send('', { embed: {
+    color: 0x9101ff,
+    title: "Tu veux m'inviter voici le lien:",
+    url: '',
+    description: `https://discordapp.com/oauth2/authorize?client_id=${client.user.id}&scope=bot&permissions=-1`,
+    footer: {
+      icon_url: client.user.avatarURL,
+      text: `${client.user.username} by shiro`
+    },
+  }})
+}
+
+module.exports.help = {
+  name: "invite"
+}
