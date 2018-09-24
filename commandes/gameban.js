39c5e863c6bc18d5201348fa12da492e58b80405
pafad <32877302+pafad@users.noreply.github.com>
2018-09-24 19:23:23 +0000

    Update gameban.js

--- commandes/gameban.js
+++ commandes/gameban.js
@@ -3,7 +3,7 @@ const request = require("request")
 
 module.exports.run = async (client, message, args) => {
 //de côté "188vrw"
-message. channel. send("indisponible
+message. channel. send("indisponible")  
 } 
 
 module.exports.help = {
