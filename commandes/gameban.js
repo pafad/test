9f8eaac35dac99ebb0402d9d97fbb7ef771c1fec
pafad <32877302+pafad@users.noreply.github.com>
2018-09-24 19:03:51 +0000

    Create gameban.js

---
+++ commandes/gameban.js
@@ -0,0 +1,11 @@
+const superagent = require("superagent") 
+const request = require("request") 
+
+module.exports.run = async (client, message, args) => {
+//de côté "188vrw"
+message. channel. send("indisponible
+} 
+
+module.exports.help = {
+name: "gameban" 
+} 
