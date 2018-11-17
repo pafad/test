731ba73515426167d7caaa630b87e655f2c80327
pafad <32877302+pafad@users.noreply.github.com>
2018-11-17 01:24:33 +0100

    Update element.js

--- commandes/element.js
+++ commandes/element.js
@@ -12,7 +12,7 @@ if(err || res.statusCode!== 200)return
     let userData = JSON.parse(body);
     let Sender = message.author;
     if(userData[Sender.id].element === "Pas choisi"){
-    let filter = (m, user) => m.content === "Feu" || m.content === "Eau" || m.content === "Air" || m.content === "Terre" || m.content === "Ténèbres" || m.content === "Lumière" && user.id === message.author.id; 
+    let filter = (message, Sender) => message.content === "Feu" || message.content === "Eau" || message.content === "Air" || message.content === "Terre" || message.content === "Ténèbres" || message.content === "Lumière" && Sender.id === message.author.id; 
     var collect = message.channel.createCollector(filter);
     message.reply("Choisi entre **Feu**, **Eau**, **Terre**, **Air**, **Ténèbres**, **Lumière**")
     collect.on("collect", m => {
