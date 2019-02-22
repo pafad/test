14cf9f3a29ba4fe15b1e2c4537fe693260652e58
pafad <32877302+pafad@users.noreply.github.com>
2019-02-22 09:30:51 +0100

    Update main.js

--- main.js
+++ main.js
@@ -89,7 +89,7 @@ if(message.content.startsWith(">hr")) {
    				 
 
    				 var time = JSON.parse(body)
-
+                                 var nombre = 360000;
    				 
 
    				 if(!time[message.author.id]) time[message.author.id] = {}
@@ -100,7 +100,7 @@ if(message.content.startsWith(">hr")) {
 
    				 
 
-   				 time[message.author.id].lastHr = new Date().getTime() +++ 360000;
+   				 time[message.author.id].lastHr = new Date().getTime() +++ nombre;
 
    				 request({ url: timeurl, method: 'PUT', json: time})
 
