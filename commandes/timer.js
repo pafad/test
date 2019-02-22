e8c2d31169d39999d9d73c80f437524163cf2092
pafad <32877302+pafad@users.noreply.github.com>
2019-02-22 12:45:14 +0100

    Update timer.js

--- commandes/timer.js
+++ commandes/timer.js
@@ -1,44 +1,32 @@
 const superagent = require("superagent") 
 const request = require("request") 
 module.exports.run= async(client, message, args) => {
-
-const timeurl = "https://api.myjson.com/bins/1dn9h4"; 
-   		 	
-   			request(timeurl, (err, res, body) => { 
-   				
-   				console.log('chargement !') 
-   				
-   				if(err || res.statusCode!== 200)return
-   				
-   				 console.log('chargé avec succés') 
-   				 
-   				 var time = JSON.parse(body)
-    
+    var time = Date.now() + 360000;
     message.channel.send("Chargement du timer...").then(m => {
     	
    	function timer() {
    
     var now = new Date().getTime();
-    var distance = time[message.author.id].lastHr - now;
+    var distance = time - now;
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);			 
     
     setTimeout(() => {
-    m.edit((time[message.author.id].lastHr < Date.now()) && (time[message.author.id].lastHr === 0) ? "Disponible" : `${message.author.username} Ton Hr : ${hours}:${minutes}:${seconds}`);
+    m.edit((time  < Date.now()) && (time === 0) ? "Disponible" : `${message.author.username} Ton Hr : ${hours}:${minutes}:${seconds}`);
     }, 250)
-    if((time[message.author.id].lastHr < Date.now()) && (time[message.author.id].lastHr === 0)){
+    if((time < Date.now()) && (time === 0)){
  
     message.channel.send(`${message.author} Ton Hr est disponible !`) 
     
     }
     
     }
-    
-    	setInterval(timer, 1250)
+
+setInterval(timer, 1250)
     	
-    	})
+    	
     
     }) 
     
