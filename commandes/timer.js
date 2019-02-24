1799eaef02fa04d87d30523e4cf90f8f0a31398b
pafad <32877302+pafad@users.noreply.github.com>
2019-02-24 12:25:15 +0100

    Update timer.js

--- commandes/timer.js
+++ commandes/timer.js
@@ -15,7 +15,7 @@ module.exports.run= async(client, message, args) => {
     
     setTimeout(() => {
     m.edit((time  < Date.now()) && (time === 0) ? "Disponible" : `${message.author.username} Ton Hr : ${hours}:${minutes}:${seconds}`);
-    }, 10)
+    }, 100)
     if((time < Date.now()) && (time === 0)){
  
     message.channel.send(`${message.author} Ton Hr est disponible !`) 
